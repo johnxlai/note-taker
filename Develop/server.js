ca9cb4202e26282ff7257e1f5e20d860eec34c5f
John Lai <lai.john.py@gmail.com>
2023-02-14 16:13:01 -0500

    Add server js file

---
+++ Develop/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const PORT = 3001;
+const app = express();
+
+//app use
+
+//Get route
+
+// app listen
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
