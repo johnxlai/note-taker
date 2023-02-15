49f01f3fd22753e0ae5a43c850e8b99c02d7cf20
John Lai <lai.john.py@gmail.com>
2023-02-14 21:17:47 -0500

    Add routes index.js so server.js has connection from notes to server js

---
+++ Develop/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+//import modular routers for notes.js
+const notesRouter = require('./notes');
+
+const app = express();
+
+app.use('/notes', notesRouter);
+
+module.exports = app;
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const api = require('./routes/index');
 
 const PORT = 3001;
 const app = express();
@@ -8,11 +9,12 @@ const app = express();
 //app use tell express to look into public for static content
 app.use(express.static('public'));
 
-//Get route
+//Get route for home page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+//Get route for notes page
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
