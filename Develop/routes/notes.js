e8dd66cf8394a3010e38e1a65802c595f12a9bb9
John Lai <lai.john.py@gmail.com>
2023-02-15 16:35:51 -0500

    Add route to db

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,5 +1,10 @@
 const notes = require('express').Router();
+const dbNotes = require('../db/db.json');
 
 //Get Routes for retrieving all notes
+notes.get('/', (req, res) => {
+  console.info(`Get /api/notes`);
+  res.status(200).json(dbNotes);
+});
 
 module.exports = notes;
