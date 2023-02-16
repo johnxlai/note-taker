530276ee27bfd44448257cd88e97385be150a246
John Lai <lai.john.py@gmail.com>
2023-02-15 22:05:54 -0500

    Add wildcard for redirect and declare var for fsReadWhite js file

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+
+//Get route for wildcard
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 // app listen
 
 app.listen(PORT, () =>
