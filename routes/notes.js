96fa64abee9e665326a0dea4601713c6e2c94507
John Lai <lai.john.py@gmail.com>
2023-02-15 23:36:25 -0500

    Add notes post for posting new note

--- routes/notes.js
+++ routes/notes.js
@@ -3,41 +3,63 @@ const dbNotes = require('../db/db.json');
 //unique id npm package
 const uniqid = require('uniqid');
 
+// fs.readFile('data.csv', 'utf8', (error, data) =>
+//   error ? console.error(error) : console.log(data)
+// );
+
 //Get Routes for retrieving all notes
 notes.get('/', (req, res) => {
-  console.info(`Get /api/notes`);
   res.status(200).json(dbNotes);
-  //read from existing file
-  const { noteTitle, noteTextArea, uniqidId = 0 } = req.body;
-
-  // Check if there is anything in the response body
-  // if (req.body && req.body.product) {
-  //   response = {
-  //     status: 'success',
-  //     data: req.body,
-  //   };
-  //   res.status(201).json(response);
-  // } else {
-  //   res.status(400).json('Request body must at least contain a product name');
-  // }
-
-  if (noteTitle && noteTextArea) {
-    const newNote = {
-      noteTitle,
-      noteTextArea,
-      noteId: uniqid(),
-    };
-    // Log the response body to the console
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-    console.log(response);
-  } else {
-    res.status(500).json(`Error in posting notes`);
-  }
+  // console.log(dbNotes);
+  return res.json(dbNotes);
 });
 
 //Post route for a new note
+notes.post('/', (req, res) => {
+  //Grab details from existing notes
+  console.info(`${req.method} request received to add a review`);
+
+  // Prepare a response object to send back to the client
+  let response;
+
+  // Check if there is anything in the response body
+  if (req.body) {
+    response = {
+      status: 'success',
+      data: req.body,
+    };
+    res.json(`Review for  has been added!`);
+  } else {
+    res.json('Request body must at least contain a product name');
+  }
+
+  // Log the response body to the console
+  console.log(req.body);
+
+  // console.info(`${req.method} request received to add a review`);
+
+  // const { title, text, uniqidId = 0 } = req.body;
+  // console.log(req.body);
+  // if (req.body && title && text) {
+  //   const newNote = {
+  //     title,
+  //     text,
+  //     uniqidId: uniqid(),
+  //   };
+  //   // Log the response body to the console
+  //   const response = {
+  //     status: 'success',
+  //     body: newNote,
+  //   };
+  //   console.log(response);
+
+  //res.status(500).json(`Error in posting notes`);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to add a note`);
+});
+
+//post route to add new note to data
+notes.post('/', (req, res) => {});
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use('/api', api);
 
 //app use tell express to look into public for static content
